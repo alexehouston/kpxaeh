024c22479dd6bb7c8a30b7ca3a978ba9979b909c
ahouston <alexehouston@gmail.com>
2023-06-21 10:23:12 -0500

    update contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <div className="Contact">
             <div className="Kristen">
-                <h3>Contact Kristen</h3>
+                <h3>Contact <span>Kristen</span></h3>
                 <div className="contact-info">
                     <a href="tel:708 351-7087"><i class="fa-solid fa-phone"></i></a>
                     <a href = "mailto:kristen@kphaircolor.com?subject = KP Hair Color Booking&body = Message"><i class="fa-solid fa-envelope"></i></a>
@@ -12,14 +12,14 @@ export default function Contact() {
                 </div>
             </div>
             <div className="Alex">
-                <h3>Contact Alex</h3>
+                <h3>Contact <span>Alex</span></h3>
                 <div className="contact-info">
                     <a href = "mailto:alex@alexehouston.com?subject = Alex E. Houston Hair Booking&body = Message"><i class="fa-solid fa-envelope"></i></a>
                     <a href="https://www.instagram.com/byalexehouston/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
                 </div>
             </div>
             <div className="Lexi">
-                <h3>Follow Lexi</h3>
+                <h3>Follow <span>Lexi</span></h3>
                 <div className="contact-info">
                 <a href="https://www.instagram.com/lexiluslooks/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
                 </div>
