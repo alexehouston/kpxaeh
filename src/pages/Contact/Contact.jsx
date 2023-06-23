7852dd8e5078a09f6fe04eb9b62d20b2bdcda80a
ahouston <alexehouston@gmail.com>
2023-06-23 09:32:42 -0500

    update contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,24 +4,24 @@ export default function Contact() {
     return (
         <div className="Contact">
             <div className="Kristen">
-                <h3>Contact <span>Kristen</span></h3>
+                <h3>Contact <span className="contact-name">Kristen</span></h3>
                 <div className="contact-info">
-                    <a href="tel:708 351-7087"><i class="fa-solid fa-phone"></i></a>
-                    <a href = "mailto:kristen@kphaircolor.com?subject=KP Hair Color Booking"><i class="fa-solid fa-envelope"></i></a>
-                    <a href="https://www.instagram.com/kphaircolor/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="tel:708 351-7087"><i class="fa-solid fa-phone"></i> <span className="contact-detail">(708) 351-7087</span></a>
+                    <a href = "mailto:kristen@kphaircolor.com?subject=KP Hair Color Booking"><i class="fa-solid fa-envelope"></i> <span className="contact-detail">kristen@kphaircolor.com</span></a>
+                    <a href="https://www.instagram.com/kphaircolor/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i> <span className="contact-detail">@kphaircolor</span></a>
                 </div>
             </div>
             <div className="Alex">
-                <h3>Contact <span>Alex</span></h3>
+                <h3>Contact <span className="contact-name">Alex</span></h3>
                 <div className="contact-info">
-                    <a href = "mailto:byalexehouston@gmail.com?subject=Alex E. Houston Hair Booking"><i class="fa-solid fa-envelope"></i></a>
-                    <a href="https://www.instagram.com/byalexehouston/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                    <a href = "mailto:byalexehouston@gmail.com?subject=Alex E. Houston Hair Booking"><i class="fa-solid fa-envelope"></i> <span className="contact-detail">byalexehouston@gmail.com</span></a>
+                    <a href="https://www.instagram.com/byalexehouston/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i> <span className="contact-detail">@byalexehouston</span></a>
                 </div>
             </div>
             <div className="Lexi">
-                <h3>Follow <span>Lexi</span></h3>
+                <h3>Follow <span className="contact-name">Lexi</span></h3>
                 <div className="contact-info">
-                <a href="https://www.instagram.com/lexiluslooks/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.instagram.com/lexiluslooks/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i> <span className="contact-detail">@lexiluslooks</span></a>
                 </div>
             </div>
         </div>
